65fa0c99713fb0d1b4cd9b907458a795556087d1
Pot8tr2 <126396683+Pot8tr2@users.noreply.github.com>
2023-02-26 06:51:47 -0700

    Add files via upload

--- camera_sending.py
+++ camera_sending.py
@@ -1,34 +1,18 @@
-# Write your code here :-)
-# import the necessary packages
-from imutils.video import VideoStream
-import imagezmq
-import argparse
-import socket
-import time
-#pip install these
+ 1   # run this program on each RPi to send a labelled image stream
+ 2   import socket
+ 3   import time
+ 4   from imutils.video import VideoStream
+ 5   import imagezmq
+ 6
+ 7   sender = imagezmq.ImageSender(connect_to='tcp://jeff-macbook:5555')
+ 8
+ 9   rpi_name = socket.gethostname() # send RPi hostname with each image
+10   picam = VideoStream(src=0).start()
+11   time.sleep(2.0)  # allow camera sensor to warm up
+12   while True:  # send images as stream until Ctrl-C
+13       image = picam.read()
+14       sender.send_image(rpi_name, image)
 
-# construct the argument parser and parse the arguments. Need server IP.
-ap = argparse.ArgumentParser()
-ap.add_argument("-s", -i"--serverp", required=True,
-	help="ip address of the server to which the client will connect")
-args = vars(ap.parse_args())
-# initialize the ImageSender object with the socket address of the
-# server
-sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
-	args["server_ip"]))
-
-# get the host name, initialize the video stream, and allow the
-# camera sensor to warmup
-rpiName = socket.gethostname()
-#vs = VideoStream(usePiCamera=False).start()
-# Get stream from webcam and set parameters)
 #vs = VideoStream(src=0).start()//source set to what the file is written to i guess. temp mem buffer.
 #vs = VideoStream(src=0, resolution=(320, 240)).start()#//other one, not certain if it works
-time.sleep(2.0)
-
-while True:
-	# read the frame from the camera and send it to the server
-	frame = vs.read()
-	sender.send_image(rpiName, frame)
-
 
--- server_code_non_bloat.py
+++ server_code_non_bloat.py
@@ -1,86 +1,9 @@
-# import the necessary packages
-from imutils import build_montages
-from datetime import datetime
-import numpy as np
-import imagezmq
-import argparse
-import imutils
-import cv2
-# construct the argument parser and parse the arguments
-
-# initialize the dictionary which will contain  information regarding
-# when a device was last active, then store the last time the check
-# was made was now
-lastActive = {}
-lastActiveCheck = datetime.now()
-# stores the estimated number of Pis, active checking period, and
-# calculates the duration seconds to wait before making a check to
-# see if a device was active
-ESTIMATED_NUM_PIS = 1
-ACTIVE_CHECK_PERIOD = 10
-ACTIVE_CHECK_SECONDS = ESTIMATED_NUM_PIS * ACTIVE_CHECK_PERIOD
-# assign montage width and height so we can view all incoming frames
-# in a single "dashboard"
-mW = args["montageW"]
-mH = args["montageH"]
-print("[INFO] detecting: {}...".format(", ".join(obj for obj in
-	CONSIDER)))
-#important things
-# start looping over all the frames
-while True:
-	# receive RPi name and frame from the RPi and acknowledge
-	# the receipt
-	(rpiName, frame) = imageHub.recv_image()
-	imageHub.send_reply(b'OK')
-	# if a device is not in the last active dictionary then it means
-	# that its a newly connected device
-	if rpiName not in lastActive.keys():
-		print("[INFO] receiving data from {}...".format(rpiName))
-	# record the last active time for the device from which we just
-	# received a frame
-	lastActive[rpiName] = datetime.now()
-
-# resize the frame to have a maximum width of 400 pixels, then
-	# grab the frame dimensions and construct a blob
-	frame = imutils.resize(frame, width=400)
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-		0.007843, (300, 300), 127.5)
-	# pass the blob through the network and obtain the detections and
-	# predictions
-	net.setInput(blob)
-	detections = net.forward()
-	# reset the object count for each object in the CONSIDER set
-	objCount = {obj: 0 for obj in CONSIDER}
-    	# draw the sending device name on the frame
-	cv2.putText(frame, rpiName, (10, 25),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	# update the new frame in the frame dictionary
-	frameDict[rpiName] = frame
-	# build a montage using images in the frame dictionary
-	montages = build_montages(frameDict.values(), (w, h), (mW, mH))
-	# display the montage(s) on the screen
-	for (i, montage) in enumerate(montages):
-		cv2.imshow("Doggo ({})".format(i),
-			montage)
-	# detect any kepresses
-	key = cv2.waitKey(1) & 0xFF
-	# if current time *minus* last time when the active device check
-	# was made is greater than the threshold set then do a check
-	if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
-		# loop over all previously active devices
-		for (rpiName, ts) in list(lastActive.items()):
-			# remove the RPi from the last active and frame
-			# dictionaries if the device hasn't been active recently
-			if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
-				print("[INFO] lost connection to {}".format(rpiName))
-				lastActive.pop(rpiName)
-				frameDict.pop(rpiName)
-		# set the last active check time as current time
-		lastActiveCheck = datetime.now()
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-# do a bit of cleanup
-cv2.destroyAllWindows()
-
+ 1   # run this program on the Mac to display image streams from multiple RPis
+ 2   import cv2
+ 3   import imagezmq
+ 4   image_hub = imagezmq.ImageHub()
+ 5   while True:  # show streamed images until Ctrl-C
+ 6       rpi_name, image = image_hub.recv_image()
+ 7       cv2.imshow(rpi_name, image) # 1 window for each RPi
+ 8       cv2.waitKey(1)
+ 9       image_hub.send_reply(b'OK')
